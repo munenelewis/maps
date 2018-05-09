fc6042782cb6ac85b919161df6ca7f1cdcdf94fc
munenelewis <munene2379@gmail.com>
2018-05-09 22:06:55 +0400

    added maps controls

--- map.py
+++ map.py
@@ -17,18 +17,23 @@ def color(elevation):
 
 map = folium.Map(location=[38.58,-99.09], zoom_start=3, tiles='Mapbox Bright')
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanos")
 
 
 
 for lt , ln , el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt,ln], popup =str(el) + 'meters', icon=folium.Icon(color=color(el))))
+    fgv.add_child(folium.Marker(location=[lt,ln], popup =str(el) + 'meters', icon=folium.Icon(color=color(el))))
+
+fgp = folium.FeatureGroup(name="Population")
 
 
-fg.add_child(folium.GeoJson(open("world.json",encoding = "utf-8-sig").read(), 
+fgp.add_child(folium.GeoJson(open("world.json",encoding = "utf-8-sig").read(), 
 style_function=lambda x: {'fillColor':'red' if x['properties']['POP2005']< 10000000 else 'black' if 10000000<= x['properties']['POP2005']<20000000 else 'green'}))
 
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+
+map.add_child(folium.LayerControl())
 
 map.save('map1.html')
 
