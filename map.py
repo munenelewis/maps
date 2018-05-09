df0f07fcc12c1446665e5612519cae87734a89a8
munenelewis <munene2379@gmail.com>
2018-05-09 20:07:23 +0400

    added multiple markers

--- map.py
+++ map.py
@@ -5,8 +5,8 @@ map = folium.Map(location=[-1.285803,36.698874], zoom_start=8, tiles='Mapbox Bri
 
 fg = folium.FeatureGroup(name="My Map")
 
-
-fg.add_child(folium.Marker(location=[-1.285803,36.698874], popup ="hi am a marker", icon=folium.Icon(color='red')))
+for coodinates in [[-1.285803,36.698874],[-2.285803,36.698874],[-3.285803,36.698874]]:
+    fg.add_child(folium.Marker(location=coodinates, popup ="hi am a marker", icon=folium.Icon(color='red')))
 
 map.add_child(fg)
 
