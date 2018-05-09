68b047fc87018a1028c19445366891cdf59bf44b
munenelewis <munene2379@gmail.com>
2018-05-09 21:06:15 +0400

    maker changes with ranges

--- map.py
+++ map.py
@@ -7,6 +7,14 @@ lon = list(data['LON'])
 lat = list(data['LAT'])
 elev = list(data['ELEV'])
 
+def color(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 map = folium.Map(location=[38.58,-99.09], zoom_start=8, tiles='Mapbox Bright')
 
 fg = folium.FeatureGroup(name="My Map")
@@ -14,7 +22,7 @@ fg = folium.FeatureGroup(name="My Map")
 
 
 for lt , ln , el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt,ln], popup =str(el) + 'meters', icon=folium.Icon(color='red')))
+    fg.add_child(folium.Marker(location=[lt,ln], popup =str(el) + 'meters', icon=folium.Icon(color=color(el))))
 
 map.add_child(fg)
 
