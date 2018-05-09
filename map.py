5bacecfb2f14e9f281e29a273c26f5050d71b02e
munenelewis <munene2379@gmail.com>
2018-05-09 20:01:53 +0400

    changed how the child were being passed as feture group

--- map.py
+++ map.py
@@ -3,8 +3,12 @@ import folium
 
 map = folium.Map(location=[-1.285803,36.698874], zoom_start=8, tiles='Mapbox Bright')
 
-map.add_child(folium.Marker(location=[-1.285803,36.698874], popup ="hi am a marker", icon=folium.Icon(color='red')))
+fg = folium.FeatureGroup(name="My Map")
 
+
+fg.add_child(folium.Marker(location=[-1.285803,36.698874], popup ="hi am a marker", icon=folium.Icon(color='red')))
+
+map.add_child(fg)
 
 map.save('map1.html')
 
