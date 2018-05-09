0dd2f5881c5153dcd5d0a1d9f044e7e35a0c55b9
munenelewis <munene2379@gmail.com>
2018-05-09 20:44:32 +0400

    imported data from csv file using pandas... looped through it using zip function and made maps makers

--- map.py
+++ map.py
@@ -1,12 +1,20 @@
 import folium
+import pandas
+
+data= pandas.read_csv('Volcanoes.txt')
+
+lon = list(data['LON'])
+lat = list(data['LAT'])
 
 
 map = folium.Map(location=[-1.285803,36.698874], zoom_start=8, tiles='Mapbox Bright')
 
 fg = folium.FeatureGroup(name="My Map")
 
-for coodinates in [[-1.285803,36.698874],[-2.285803,36.698874],[-3.285803,36.698874]]:
-    fg.add_child(folium.Marker(location=coodinates, popup ="hi am a marker", icon=folium.Icon(color='red')))
+
+
+for lt , ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt,ln], popup ="hi am a marker", icon=folium.Icon(color='red')))
 
 map.add_child(fg)
 
