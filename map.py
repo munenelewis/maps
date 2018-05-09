e8fda9be35741c57a68b4ad9b2dd3648001b1407
munenelewis <munene2379@gmail.com>
2018-05-09 19:59:43 +0400

    added makers to my map

--- map.py
+++ map.py
@@ -1,6 +1,10 @@
 import folium
 
 
-map = folium.Map(location=[-1.285803, 36.698874], zoom_start=6, tiles='Mapbox Bright')
+map = folium.Map(location=[-1.285803,36.698874], zoom_start=8, tiles='Mapbox Bright')
+
+map.add_child(folium.Marker(location=[-1.285803,36.698874], popup ="hi am a marker", icon=folium.Icon(color='red')))
+
+
 map.save('map1.html')
 
